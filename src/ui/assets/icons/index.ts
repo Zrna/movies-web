28e28bb656c24b07dc560567fb6a000255db0e53
zrna <lukazrnic5@gmail.com>
2021-05-20 16:03:20 +0200

    add icons to `ui/assets`

---
+++ src/ui/assets/icons/index.ts
@@ -0,0 +1,7 @@
+// Icons from https://heroicons.dev/
+
+export { default as arrowDown } from './arrow-down.svg';
+export { default as arrowLeft } from './arrow-left.svg';
+export { default as arrowRight } from './arrow-right.svg';
+export { default as arrowUp } from './arrow-up.svg';
+export { default as videoCamera } from './video-camera.svg';
