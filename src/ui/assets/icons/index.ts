63ef1b6c7580f5f61a81578aaa3101a98f5e688d
zrna <lukazrnic5@gmail.com>
2021-06-02 15:26:41 +0200

    add new icons

--- src/ui/assets/icons/index.ts
+++ src/ui/assets/icons/index.ts
@@ -4,4 +4,7 @@ export { default as arrowDown } from './arrow-down.svg';
 export { default as arrowLeft } from './arrow-left.svg';
 export { default as arrowRight } from './arrow-right.svg';
 export { default as arrowUp } from './arrow-up.svg';
+export { default as close } from './close.svg';
+export { default as eye } from './eye.svg';
+export { default as eyeOff } from './eye-off.svg';
 export { default as videoCamera } from './video-camera.svg';
