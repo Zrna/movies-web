f4509891d39f125c2d193e144d2ad9d2dd3af555
zrna <lukazrnic5@gmail.com>
2021-06-02 15:39:32 +0200

    set max app width to 1920px

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,14 +1,16 @@
 import { Route, Switch } from 'react-router-dom';
 
 import { HomePage, LoginPage } from '~/pages';
-import { Text } from '~/ui';
+import { FlexLayout, Text } from '~/ui';
 
 export const Routes = () => {
   return (
-    <Switch>
-      <Route component={LoginPage} exact path="/login" />
-      <Route component={HomePage} exact path="/" />
-      <Route path="*" render={() => <Text variant="display-heading-xl">404 Page Not Found</Text>} />
-    </Switch>
+    <FlexLayout flexDirection="column" sx={{ maxWidth: '1920px', margin: '0 auto' }}>
+      <Switch>
+        <Route component={LoginPage} exact path="/login" />
+        <Route component={HomePage} exact path="/" />
+        <Route path="*" render={() => <Text variant="display-heading-xl">404 Page Not Found</Text>} />
+      </Switch>
+    </FlexLayout>
   );
 };
