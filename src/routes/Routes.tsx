76ccb22d01b103aeccb8a69e46fe629825b1e7e0
zrna <lukazrnic5@gmail.com>
2021-06-04 14:03:35 +0200

    add initial Dashboard route

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -7,6 +7,14 @@ export const Routes = () => {
   return (
     <FlexLayout flexDirection="column" sx={{ maxWidth: '1920px', margin: '0 auto' }}>
       <Switch>
+        <Route
+          path="/dashboard"
+          render={() => (
+            <Text color="primary" variant="display-heading-m">
+              Dashboard
+            </Text>
+          )}
+        />
         <Route component={LoginPage} exact path="/login" />
         <Route component={HomePage} exact path="/" />
         <Route path="*" render={() => <Text variant="display-heading-xl">404 Page Not Found</Text>} />
