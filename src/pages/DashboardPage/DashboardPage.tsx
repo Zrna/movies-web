55d0005f3265390534b7580f2d232f1962ce1b51
zrna <lukazrnic5@gmail.com>
2021-06-04 16:28:17 +0200

    add logout button

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,9 +1,22 @@
-import { Text } from '~/ui';
+import Cookies from 'js-cookie';
+import { useHistory } from 'react-router';
+
+import { Button, FlexLayout, Text } from '~/ui';
 
 export const DashboardPage = () => {
+  const history = useHistory();
+
+  const handleLogout = () => {
+    Cookies.remove('access-token');
+    history.push('/login');
+  };
+
   return (
-    <Text color="primary" variant="display-heading-m">
-      Dashboard
-    </Text>
+    <FlexLayout flexDirection="column" p={4} space={6}>
+      <Text color="primary" variant="display-heading-m">
+        Dashboard
+      </Text>
+      <Button text="Logout" onClick={handleLogout} />
+    </FlexLayout>
   );
 };
