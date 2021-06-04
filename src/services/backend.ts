82241835478bacf7b39c948595caf6e99d51fe3f
zrna <lukazrnic5@gmail.com>
2021-06-04 13:48:46 +0200

    add axios and backend service

---
+++ src/services/backend.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosRequestConfig } from 'axios';
+
+type AxiosFunction = <T>(url: string, config?: AxiosRequestConfig) => Promise<T>;
+type AxiosDataFunction = <T>(url: string, data?: any, config?: AxiosRequestConfig) => Promise<T>;
+
+export function backendService() {
+  const baseURL = process.env.NODE_ENV === 'production' ? 'http://localhost:5000' : 'http://localhost:5000';
+
+  const instance = axios.create({
+    baseURL,
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+  });
+
+  instance.interceptors.request.use((config) => {
+    config.withCredentials = true;
+    return config;
+  });
+
+  return {
+    get: instance.get as AxiosFunction,
+    post: instance.post as AxiosDataFunction,
+    delete: instance.delete as AxiosFunction,
+    put: instance.put as AxiosDataFunction,
+    patch: instance.patch as AxiosDataFunction,
+  };
+}
+
+export const backend = backendService();
