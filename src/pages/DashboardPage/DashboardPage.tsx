655dd920f1a8cabaf495ee22fc0c2abb51cd6202
zrna <lukazrnic5@gmail.com>
2021-06-05 20:12:00 +0200

    invalidate `account` data on logout and add link to `/account` on Dashboard page

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,13 +1,17 @@
 import Cookies from 'js-cookie';
-import { useHistory } from 'react-router';
+import { useQueryClient } from 'react-query';
+import { useHistory } from 'react-router-dom';
 
+import { ButtonLink } from '~/components';
 import { Button, FlexLayout, Text } from '~/ui';
 
 export const DashboardPage = () => {
   const history = useHistory();
+  const queryClient = useQueryClient();
 
   const handleLogout = () => {
     Cookies.remove('access-token');
+    queryClient.invalidateQueries('account');
     history.push('/login');
   };
 
@@ -16,7 +20,10 @@ export const DashboardPage = () => {
       <Text color="primary" variant="display-heading-m">
         Dashboard
       </Text>
-      <Button text="Logout" onClick={handleLogout} />
+      <FlexLayout flexDirection="row" space={5}>
+        <ButtonLink text="Account" to="/account" />
+        <Button text="Logout" onClick={handleLogout} />
+      </FlexLayout>
     </FlexLayout>
   );
 };
