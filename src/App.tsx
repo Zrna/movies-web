7f76cb1e149f3b374097155ddc4ad4feb035669e
zrna <lukazrnic5@gmail.com>
2021-06-06 15:20:08 +0200

    add ToastContainer component to the app root (App.js)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { ToastContainer } from 'react-toastify';
+
 import { Routes } from './routes';
 
 function App() {
-  return <Routes />;
+  return (
+    <>
+      <Routes />
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App;
