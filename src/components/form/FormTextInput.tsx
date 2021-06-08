4aac4261a61304de0081abe9d9f283f7f71de9ae
zrna <lukazrnic5@gmail.com>
2021-06-08 10:23:16 +0200

    improve `FormTextInput` component

--- src/components/form/FormTextInput.tsx
+++ src/components/form/FormTextInput.tsx
@@ -1,18 +1,24 @@
 import { Field } from 'react-final-form';
 
-import { TextInput } from '~/ui';
+import { TextInput, TextInputProps } from '~/ui';
 
-export interface FormTextInputProps {
+export interface FormTextInputProps extends Omit<TextInputProps, 'value' | 'onChange'> {
   name: string;
   type?: string;
   label?: string;
+  value?: null;
+  onChange?(): null;
 }
 
-export const FormTextInput: React.FC<FormTextInputProps> = ({ name, label, type = 'text' }) => {
+export const FormTextInput: React.FC<FormTextInputProps> = ({ name, label, type = 'text', ...rest }) => {
+  // `value` and `onChange` from the `rest` are ignored,
+  // because `<Field />` component has it's own `value` and `onChange`
+  const { value: ignoredValue, onChange: ignoredOnChange, ...newRest } = rest;
+
   return (
     <Field name={name}>
       {({ input: { name, value, onChange } }) => (
-        <TextInput label={label} name={name} type={type} value={value} onChange={onChange} />
+        <TextInput label={label} name={name} type={type} value={value} onChange={onChange} {...newRest} />
       )}
     </Field>
   );
