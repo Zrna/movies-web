6dffb84e963e30ff9c246f6b66bb03475d381e23
zrna <lukazrnic5@gmail.com>
2021-06-08 16:03:42 +0200

    add Login and Register buttons on Home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,21 @@
-import { Redirect } from 'react-router-dom';
+import { ButtonLink } from '~/components';
+import { FlexLayout, Text } from '~/ui';
+
+const btnStyle = {
+  width: '110px',
+  height: '50px',
+  justifyContent: 'center',
+  cursor: 'pointer',
+};
 
 export const HomePage = () => {
-  return <Redirect to="/login" />;
+  return (
+    <FlexLayout alignItems="center" flexDirection="column" space={5} sx={{ marginTop: '30vh' }}>
+      <ButtonLink sx={btnStyle} text="Login" to="/login" variant="secondary" />
+      <Text color="white" variant="text-xl-medium">
+        or
+      </Text>
+      <ButtonLink sx={btnStyle} text="Register" to="/register" variant="secondary" />
+    </FlexLayout>
+  );
 };
