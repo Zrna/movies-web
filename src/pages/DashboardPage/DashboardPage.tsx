14a465cb29139bb01861661a1e914ba777b16f8e
zrna <lukazrnic5@gmail.com>
2021-06-12 16:20:49 +0200

    remove Account and Logout buttons from Dashboard page

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,19 +1,11 @@
-import { ButtonLink } from '~/components';
-import { useLogout } from '~/hooks';
-import { Button, FlexLayout, Text } from '~/ui';
+import { FlexLayout, Text } from '~/ui';
 
 export const DashboardPage = () => {
-  const logout = useLogout();
-
   return (
     <FlexLayout flexDirection="column" p={4} space={6}>
       <Text color="primary" variant="display-heading-m">
         Dashboard
       </Text>
-      <FlexLayout flexDirection="row" space={5}>
-        <ButtonLink text="Account" to="/account" />
-        <Button text="Logout" onClick={logout} />
-      </FlexLayout>
     </FlexLayout>
   );
 };
