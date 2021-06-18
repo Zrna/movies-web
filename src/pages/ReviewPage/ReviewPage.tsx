31b7924e8458e498209d7a7311b1b95bb8904962
zrna <lukazrnic5@gmail.com>
2021-06-18 14:24:49 +0200

    add placeholders for edit, delete and last updated message on Review page

--- src/pages/ReviewPage/ReviewPage.tsx
+++ src/pages/ReviewPage/ReviewPage.tsx
@@ -24,12 +24,30 @@ export const ReviewPage = () => {
     return <Redirect to="/dashboard" />;
   }
 
-  const { name } = review;
+  const { name, updatedAt } = review;
 
   return (
     <FlexLayout flexDirection="column" p={4} space={5}>
       <BackToLink text="back to dashboard" to="/dashboard" />
-      <Text variant="display-heading-m">{name}</Text>
+      <FlexLayout flexDirection="column" space={3}>
+        <Text variant="display-heading-m">{name}</Text>
+        <FlexLayout flexDirection="row" space={4}>
+          <Text color="white-alpha-75" variant="text-s-medium" onClick={() => undefined}>
+            Edit
+          </Text>
+          <Text variant="text-s-medium">|</Text>
+          <Text
+            color="white-alpha-75"
+            variant="text-s-medium"
+            onClick={() => window.confirm('Are you sure you want to delete this review?')}
+          >
+            Delete
+          </Text>
+        </FlexLayout>
+        <Text color="white-alpha-50" variant="text-s-medium">
+          <i>Last updated on {updatedAt.slice(0, 10)}</i>
+        </Text>
+      </FlexLayout>
       <FlexLayout flexDirection={['column', 'row']} space={5}>
         <Image />
         <Content data={review} />
