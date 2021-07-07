64a31f93c96a2328c9e7745969b7f101f9cf4026
zrna <lukazrnic5@gmail.com>
2021-07-07 13:03:35 +0200

    update Rating Stars component empty star color

--- src/components/RatingStars.tsx
+++ src/components/RatingStars.tsx
@@ -11,7 +11,7 @@ interface RatingStarsProps {
 export const RatingStars: React.FC<RatingStarsProps> = ({ isReadOnly = false, rating, onChange }) => {
   return (
     <Rating
-      emptySymbol={<Icon icon="star" size="l" />}
+      emptySymbol={<Icon color="white-alpha-75" icon="star" size="l" />}
       fullSymbol={<Icon icon="starFull" size="l" />}
       initialRating={rating ?? 0}
       readonly={isReadOnly}
