aa914c178b62109d4bae6e05db6c173af5bd5c25
zrna <lukazrnic5@gmail.com>
2021-07-07 13:47:18 +0200

    add size prop to RatingStars component

--- src/components/RatingStars.tsx
+++ src/components/RatingStars.tsx
@@ -5,14 +5,15 @@ import { Icon } from '~/ui';
 interface RatingStarsProps {
   isReadOnly?: boolean;
   rating: number | null;
+  size?: 'm' | 'l' | 'xl';
   onChange?(value: number): void;
 }
 
-export const RatingStars: React.FC<RatingStarsProps> = ({ isReadOnly = false, rating, onChange }) => {
+export const RatingStars: React.FC<RatingStarsProps> = ({ isReadOnly = false, rating, size = 'l', onChange }) => {
   return (
     <Rating
-      emptySymbol={<Icon color="white-alpha-75" icon="star" size="l" />}
-      fullSymbol={<Icon icon="starFull" size="l" />}
+      emptySymbol={<Icon color="white-alpha-75" icon="star" size={size} />}
+      fullSymbol={<Icon icon="starFull" size={size} />}
       initialRating={rating ?? 0}
       readonly={isReadOnly}
       onChange={onChange}
