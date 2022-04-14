830069fa0d5ab16054d178dccb2d20e96b4422de
zrna <lukazrnic5@gmail.com>
2022-04-14 12:19:48 +0200

    change api endpoints from movies to reviews

--- src/api/reviews.ts
+++ src/api/reviews.ts
@@ -3,21 +3,21 @@ import { backend } from '~/services';
 import { CreateReview, GetReviews, Review, UpdateReview } from './reviews.d';
 
 export const getReviews = async () => {
-  return await backend.get<GetReviews>('/api/movies');
+  return await backend.get<GetReviews>('/api/reviews');
 };
 
 export const createReview = async (data: CreateReview) => {
-  return await backend.post<Review>('/api/movies', data);
+  return await backend.post<Review>('/api/reviews', data);
 };
 
 export const getReviewById = async (id: string) => {
-  return await backend.get<Review>(`/api/movies/${id}`);
+  return await backend.get<Review>(`/api/reviews/${id}`);
 };
 
 export const updateReviewById = async ({ id, data }: { id: string; data: UpdateReview }) => {
-  return await backend.put<Review>(`/api/movies/${id}`, data);
+  return await backend.put<Review>(`/api/reviews/${id}`, data);
 };
 
 export const deleteReviewById = async (id: string) => {
-  return await backend.delete<true>(`/api/movies/${id}`);
+  return await backend.delete<true>(`/api/reviews/${id}`);
 };
