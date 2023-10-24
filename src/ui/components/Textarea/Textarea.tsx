35c6adbeddde6c96ef9e2dacf3184614f9e22f27
zrna <lukazrnic5@gmail.com>
2023-10-24 20:04:09 +0200

    update ui/Textarea component

--- src/ui/components/Textarea/Textarea.tsx
+++ src/ui/components/Textarea/Textarea.tsx
@@ -1,9 +1,10 @@
 import { Textarea as ThemeTextarea } from 'theme-ui';
 
-import { Text, theme } from '~/ui';
+import { FlexLayout, Text, theme } from '~/ui';
 
 interface TextareaProps {
   error?: string;
+  helperText?: string;
   isDisabled?: boolean;
   label?: string;
   labelColor?: theme.Color;
@@ -18,6 +19,7 @@ interface TextareaProps {
 
 export const Textarea: React.FC<TextareaProps> = ({
   error,
+  helperText,
   isDisabled = false,
   label,
   labelColor = 'white-alpha-75',
@@ -28,41 +30,40 @@ export const Textarea: React.FC<TextareaProps> = ({
   ...rest
 }) => {
   return (
-    <>
+    <FlexLayout flexDirection="column" space={2}>
       {label && (
-        <Text as="label" color={labelColor}>
+        <Text as="label" color={error ? 'alert-error' : labelColor} variant="paragraph-default">
           {label}
         </Text>
       )}
       <ThemeTextarea
-        bg="dimmed"
+        bg="dark"
         disabled={isDisabled}
         opacity={isDisabled ? 0.5 : 1}
         p={4}
         placeholder={placeholder}
         rows={rows}
         sx={{
-          border: error ? `1px solid ${theme.colors['alert-error']}` : 'none',
-          borderRadius: 's',
+          border: error ? `1px solid ${theme.colors['alert-error']}` : `1px solid ${theme.colors['light-dark']}`,
+          borderRadius: '8px',
           outline: 'none',
           '::placeholder': {
-            color: 'white-alpha-50',
-          },
-          '&:hover': {
-            backgroundColor: 'dimmed',
-          },
-          '&:focus-within': {
-            backgroundColor: 'dimmed',
+            color: 'dimmed',
           },
           maxWidth: '100%',
           minWidth: '100%',
         }}
         value={value}
-        variant="text.text-m"
+        variant="text.paragraph-default"
         onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => onChange(e.target.value)}
         {...rest}
       />
+      {helperText && (
+        <Text color="dimmed" variant="paragraph-small">
+          {helperText}
+        </Text>
+      )}
       {error && <Text color="alert-error">{error}</Text>}
-    </>
+    </FlexLayout>
   );
 };
