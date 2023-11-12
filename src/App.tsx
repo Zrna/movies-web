d65dd8270403383d8dc7d681180f24575ab52d18
zrna <lukazrnic5@gmail.com>
2023-11-12 11:06:34 +0100

    add App max width

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ToastContainer } from 'react-toastify';
 
 import { Navbar } from '~/components';
 import { Routes } from '~/routes';
+import { FlexLayout } from '~/ui';
 
 import { SearchProvider, SelectedTabFilterProvider } from './context';
 
@@ -9,9 +10,13 @@ function App() {
   return (
     <SelectedTabFilterProvider>
       <SearchProvider>
-        <Navbar />
-        <Routes />
-        <ToastContainer />
+        <FlexLayout flexDirection="column" sx={{ maxWidth: '1920px', margin: '0 auto' }}>
+          <Navbar />
+          <FlexLayout flexDirection="column" sx={{ maxWidth: '1320px', margin: '0 auto' }}>
+            <Routes />
+          </FlexLayout>
+          <ToastContainer />
+        </FlexLayout>
       </SearchProvider>
     </SelectedTabFilterProvider>
   );
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -10,23 +10,20 @@ import {
   RegistrationPage,
   ReviewPage,
 } from '~/pages';
-import { FlexLayout } from '~/ui';
 
 import { ProtectedRoute } from './ProtectedRoute';
 
 export const Routes = () => {
   return (
-    <FlexLayout flexDirection="column" sx={{ maxWidth: '1920px', margin: '0 auto' }}>
-      <Switch>
-        <ProtectedRoute component={AccountPage} exact path="/account" />
-        <ProtectedRoute component={CreateReviewPage} exact path="/create-review" />
-        <ProtectedRoute component={ReviewPage} exact path="/review/:reviewId" />
-        <ProtectedRoute component={DashboardPage} exact path="/dashboard" />
-        <Route component={RegistrationPage} exact path="/register" />
-        <Route component={LoginPage} exact path="/login" />
-        <Route component={HomePage} exact path="/" />
-        <Route component={NotFoundPage} path="*" />
-      </Switch>
-    </FlexLayout>
+    <Switch>
+      <ProtectedRoute component={AccountPage} exact path="/account" />
+      <ProtectedRoute component={CreateReviewPage} exact path="/create-review" />
+      <ProtectedRoute component={ReviewPage} exact path="/review/:reviewId" />
+      <ProtectedRoute component={DashboardPage} exact path="/dashboard" />
+      <Route component={RegistrationPage} exact path="/register" />
+      <Route component={LoginPage} exact path="/login" />
+      <Route component={HomePage} exact path="/" />
+      <Route component={NotFoundPage} path="*" />
+    </Switch>
   );
 };
