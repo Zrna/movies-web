4f67f0c9edd0622b73141a077c4119896bac68f2
zrna <lukazrnic5@gmail.com>
2023-11-26 15:40:49 +0100

    update the ButtonLink component

--- src/components/ButtonLink/ButtonLink.tsx
+++ src/components/ButtonLink/ButtonLink.tsx
@@ -1,12 +1,23 @@
 import { Link } from 'react-router-dom';
 
 import { Button, ButtonProps } from '~/ui';
+import { isExternalUrl } from '~/utils';
 
 interface ButtonLinkProps extends ButtonProps {
   to: string;
 }
 
 export const ButtonLink: React.FC<ButtonLinkProps> = ({ to, text, onClick, ...rest }) => {
+  const isUrlExternal = isExternalUrl(to);
+
+  if (isUrlExternal) {
+    return (
+      <a href={to} rel="noreferrer" target="_blank">
+        <Button text={text} onClick={onClick} {...rest} />
+      </a>
+    );
+  }
+
   return (
     <Link data-testid="button-link-component" to={to}>
       <Button text={text} onClick={onClick} {...rest} />
