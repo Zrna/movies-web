e00c297bd7f599e339c96462aac06861d43eed6a
zrna <lukazrnic5@gmail.com>
2023-11-28 10:27:34 +0100

    add streaming app link

--- src/pages/DashboardPage/SideContainer/StreamingApps.tsx
+++ src/pages/DashboardPage/SideContainer/StreamingApps.tsx
@@ -2,9 +2,19 @@ import { FlexLayout, Text, theme } from '~/ui';
 import hboMax from '~/ui/assets/icons/hbo-max.svg';
 import netflix from '~/ui/assets/icons/netflix.svg';
 
-const platformLogoStyle = {
-  width: '48px',
-  height: '48px',
+const StreamingApp = ({ img, link }: { img: string; link: string }) => {
+  return (
+    <a href={link} rel="noreferrer" target="_blank">
+      <img
+        alt="Streaming App Logo"
+        src={img}
+        style={{
+          width: '48px',
+          height: '48px',
+        }}
+      />
+    </a>
+  );
 };
 
 export const StreamingApps = () => {
@@ -17,8 +27,8 @@ export const StreamingApps = () => {
     >
       <Text variant="headline-h5">Your Streaming Apps</Text>
       <FlexLayout space={2}>
-        <img alt="Netflix Logo" src={netflix} style={platformLogoStyle} />
-        <img alt="HBO Max Logo" src={hboMax} style={platformLogoStyle} />
+        <StreamingApp img={netflix} link="https://www.netflix.com/" />
+        <StreamingApp img={hboMax} link="https://play.hbomax.com/" />
       </FlexLayout>
     </FlexLayout>
   );
