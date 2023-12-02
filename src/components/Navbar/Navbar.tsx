2e29ce6e960ada3c929a89b90d843528c87763fd
zrna <lukazrnic5@gmail.com>
2023-12-02 17:13:58 +0100

    improve navbar - user menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,53 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
-import { TextWithIcon } from '~/components';
+import { AccountData } from '~/api';
 import { useSearch } from '~/context';
 import { useAccount } from '~/hooks';
-import { Box, FlexLayout, Icon, Only, Search, theme, useScreenType } from '~/ui';
+import { Box, FlexLayout, Icon, Only, Search, Text, theme, useScreenType } from '~/ui';
+import avatar from '~/ui/assets/images/avatar-15.svg';
 import logo from '~/ui/assets/images/logo.svg';
 
 import { AccountMenu } from './AccountMenu';
 import { Tabs } from './Tabs';
+
+interface UserMenuProps {
+  name: string;
+  isOpen: boolean;
+  onClick: () => void;
+}
+
+const UserMenu: React.FC<UserMenuProps> = ({ name, isOpen, onClick }) => {
+  const { isMobile } = useScreenType();
+
+  return (
+    <FlexLayout
+      alignItems="center"
+      px={2}
+      space={3}
+      sx={{
+        width: 'auto',
+        height: ['40px', '50px'],
+        wordBreak: 'break-all',
+        borderRadius: '50px',
+        border: `2px solid ${theme.colors['light-dark']}`,
+      }}
+      onClick={onClick}
+    >
+      <img
+        alt="Avatar"
+        src={avatar}
+        style={{ width: isMobile ? '24px' : '36px', height: isMobile ? '24px' : '36px' }}
+      />
+      {!isMobile && <Text color="dimmed">{name}</Text>}
+      <Icon icon={isOpen ? 'chevronUp' : 'chevronDown'} />
+    </FlexLayout>
+  );
+};
+
+const getUserInitials = (account: AccountData) => {
+  return `${account.firstName.charAt(0)}${account.lastName.charAt(0)}`;
+};
 
 export const Navbar = () => {
   const { pathname } = useLocation();
@@ -17,6 +56,7 @@ export const Navbar = () => {
   const { search, setSearch } = useSearch();
 
   const [isAccountMenuOpen, setIsAccountMenuOpen] = useState(false);
+  const isDashboard = pathname === '/dashboard';
 
   useEffect(() => {
     setIsAccountMenuOpen(false);
@@ -34,57 +74,24 @@ export const Navbar = () => {
         justifyContent="space-between"
         p={[2, 4]}
         space={4}
-        sx={{ maxWidth: '1920px', margin: '0 auto' }}
+        sx={{ maxWidth: '1920px', margin: '0 auto', height: ['50', '90px'] }}
       >
         <Link to="/dashboard">
           <img alt="Moovier logo" src={logo} style={{ width: isDesktop ? '100%' : '100px' }} />
         </Link>
-        {pathname === '/dashboard' && (
+        {isDashboard && (
           <Only for="desktop">
             <Tabs tabs={['All Reviews', 'Watch Again', 'Bucket List']} />
           </Only>
         )}
-        <>
-          {isMobile ? (
-            <FlexLayout space={2}>
-              <Search isMini value={search} onChange={setSearch} />
-              <FlexLayout
-                alignItems="center"
-                flexDirection="row"
-                px={2}
-                py={1}
-                space={2}
-                sx={{
-                  borderRadius: '50px',
-                  border: `1px solid ${theme.colors['light-dark']}`,
-                }}
-                onClick={() => setIsAccountMenuOpen(!isAccountMenuOpen)}
-              >
-                <Icon color="white-alpha-75" icon="user" size="l" />
-                <Icon icon={isAccountMenuOpen ? 'chevronUp' : 'chevronDown'} />
-              </FlexLayout>
-            </FlexLayout>
-          ) : (
-            <FlexLayout space={5}>
-              <Search value={search} onChange={setSearch} />
-              <TextWithIcon
-                iconLeft="user"
-                iconRight={isAccountMenuOpen ? 'chevronUp' : 'chevronDown'}
-                space={3}
-                sx={{
-                  width: ['auto', 'auto', '130px'],
-                  wordBreak: 'break-all',
-                  borderRadius: '50px',
-                  border: `1px solid ${theme.colors['light-dark']}`,
-                  paddingX: 4,
-                  paddingY: 1,
-                }}
-                text={account.firstName}
-                onClick={() => setIsAccountMenuOpen(!isAccountMenuOpen)}
-              />
-            </FlexLayout>
-          )}
-        </>
+        <FlexLayout space={[2, 5]}>
+          {isDashboard && <Search isMini={isMobile} value={search} onChange={setSearch} />}
+          <UserMenu
+            isOpen={isAccountMenuOpen}
+            name={getUserInitials(account)}
+            onClick={() => setIsAccountMenuOpen(!isAccountMenuOpen)}
+          />
+        </FlexLayout>
       </FlexLayout>
       {isAccountMenuOpen && <AccountMenu onOutsideClick={() => setIsAccountMenuOpen(false)} />}
     </Box>
