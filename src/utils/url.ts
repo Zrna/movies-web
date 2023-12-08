8c4e3f981f6ab83262a0b21375996a8cafae5c55
zrna <lukazrnic5@gmail.com>
2023-12-08 15:07:12 +0100

    add new url utils functions

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,3 +1,11 @@
 export function isExternalUrl(href: string) {
   return href.startsWith('http') || href.startsWith('mailto:');
 }
+
+export function getUrlDomain(url: string | URL) {
+  return url.toString().replace(/.+\/\/|www.|\..+/g, '');
+}
+
+export const addUrlProtocol = (url: string) => {
+  return url.startsWith('http') ? url : `https://${url}`;
+};
