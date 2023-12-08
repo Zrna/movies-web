70d290207777da827f23795cf58a442f38ee6879
zrna <lukazrnic5@gmail.com>
2023-12-08 15:08:23 +0100

    improve the StreamingApp component

--- src/components/StreamingApp.tsx
+++ src/components/StreamingApp.tsx
@@ -1,7 +1,11 @@
+import capitalize from 'lodash/capitalize';
+
+import { FlexLayout, Text, useScreenType } from '~/ui';
 import hbomax from '~/ui/assets/icons/hbo-max.svg';
 import netflixImg from '~/ui/assets/icons/netflix.svg';
+import { addUrlProtocol } from '~/utils';
 
-const streamingAppsMap = {
+const streamingAppsMap: { [key: string]: { link: string; img: string } } = {
   netflix: {
     link: 'https://www.netflix.com/',
     img: netflixImg,
@@ -12,23 +16,46 @@ const streamingAppsMap = {
   },
 };
 
-export const StreamingApp = ({ name }: { name: keyof typeof streamingAppsMap }) => {
-  const app = streamingAppsMap[name] ?? null;
+interface StreamingAppProps {
+  name: string | null | undefined;
+  link?: string | null | undefined;
+  showName?: boolean;
+}
 
-  if (!app) {
+export const StreamingApp: React.FC<StreamingAppProps> = ({ name, link, showName = false }) => {
+  const { isMobile } = useScreenType();
+
+  if (!name) {
     return null;
   }
 
-  return (
-    <a href={app.link} rel="noreferrer" target="_blank">
-      <img
-        alt="Streaming App Logo"
-        src={app.img}
-        style={{
-          width: '48px',
-          height: '48px',
-        }}
-      />
-    </a>
-  );
+  const app = streamingAppsMap[name];
+
+  if (app) {
+    return (
+      <a href={link ? addUrlProtocol(link) : app.link} rel="noreferrer" target="_blank">
+        <FlexLayout alignItems="center" space={4}>
+          <img
+            alt="Streaming App Logo"
+            src={app.img}
+            style={{
+              width: isMobile ? '32px' : '48px',
+              height: isMobile ? '32px' : '48px',
+            }}
+          />
+          {showName && <Text variant="paragraph-default">{capitalize(name)}</Text>}
+        </FlexLayout>
+      </a>
+    );
+  }
+
+  if (name && showName) {
+    return (
+      <a href={link ? addUrlProtocol(link) : ''} rel="noreferrer" target="_blank">
+        <Text variant="paragraph-default">{capitalize(name)}</Text>
+      </a>
+    );
+  }
+
+  return null;
 };
