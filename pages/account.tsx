bb716a07622678bbf7cc059541edeb8673bb397f
zrna <lukazrnic5@gmail.com>
2024-10-18 16:40:17 +0200

    refactor the account page to use react-hook-form and zod

--- pages/account.tsx
+++ pages/account.tsx
@@ -1,18 +1,25 @@
-import isEqual from 'lodash/isEqual';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useEffect } from 'react';
-import { Form } from 'react-final-form';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 
-import { BackToLink, FormTextInput } from '~/components';
+import { BackToLink } from '~/components';
+import { FormTextInput } from '~/components/form-new';
 import { useAccount, useDeleteAccount, useLogout, useUpdateAccount } from '~/hooks';
-import { Button, FlexLayout, Text, useModal, useScreenType } from '~/ui';
-import { validator } from '~/utils';
+import { UpdateAccountArgs } from '~/interfaces/account';
+import { Button, FlexLayout, Text, TextInput, useModal, useScreenType } from '~/ui';
+
+const AccountFormSchema = z.object({
+  firstName: z.string().min(2, 'First name must contain at least 2 characters'),
+  lastName: z.string().min(2, 'Last name must contain at least 2 characters'),
+});
 
 export default function AccountPage() {
   const logout = useLogout();
-
   const { data: account, error, isLoading } = useAccount();
   const { mutate: updateAccount } = useUpdateAccount();
   const { mutate: deleteAccount } = useDeleteAccount();
+
   const { isMobile } = useScreenType();
   const [modal, showModal] = useModal({
     title: 'Delete account',
@@ -21,6 +28,18 @@ export default function AccountPage() {
       text: 'Delete account',
       action: async () => deleteAccount(),
     },
+  });
+  const {
+    control,
+    handleSubmit,
+    formState: { isSubmitting, isDirty },
+  } = useForm<UpdateAccountArgs>({
+    resolver: zodResolver(AccountFormSchema),
+    defaultValues: {
+      firstName: '',
+      lastName: '',
+    },
+    values: account,
   });
 
   useEffect(() => {
@@ -33,48 +52,29 @@ export default function AccountPage() {
     return null;
   }
 
-  const { email, firstName, lastName } = account;
-
   return (
     <FlexLayout flexDirection="column" p={4} space={6}>
       <BackToLink text="Back to All Reviews" to="/dashboard" />
       <Text variant="headline-h2">Account</Text>
-      <Form
-        initialValues={{ email, firstName, lastName }}
-        render={({ handleSubmit, hasValidationErrors, submitting, initialValues, values }) => {
-          const valuesNotChanged = isEqual(initialValues, values);
-
-          return (
-            <FlexLayout
-              as="form"
-              flexDirection="column"
-              space={5}
-              sx={{ width: ['100%', '500px'] }}
-              onSubmit={handleSubmit}
-            >
-              <FormTextInput
-                label="First name"
-                name="firstName"
-                validate={validator.isEmpty("First name can't be empty")}
-              />
-              <FormTextInput
-                label="Last name"
-                name="lastName"
-                validate={validator.isEmpty("Last name can't be empty")}
-              />
-              <FormTextInput iconRight="lock" isDisabled label="Email" name="email" />
-              <Button
-                isDisabled={valuesNotChanged || hasValidationErrors}
-                isFullWidth={isMobile}
-                isLoading={submitting}
-                text="Update account"
-                type="submit"
-              />
-            </FlexLayout>
-          );
-        }}
-        onSubmit={(data: any) => updateAccount(data)}
-      />
+      <FlexLayout as="form" flexDirection="column" space={5} sx={{ width: ['100%', '500px'] }}>
+        <FormTextInput control={control} label="First name" name="firstName" />
+        <FormTextInput control={control} label="Last name" name="lastName" />
+        <TextInput
+          iconRight="lock"
+          isDisabled
+          label="Email"
+          name="email"
+          value={account.email || ''}
+          onChange={() => undefined}
+        />
+        <Button
+          isFullWidth={isMobile}
+          isLoading={isSubmitting}
+          text="Update account"
+          type="submit"
+          onClick={handleSubmit((data) => isDirty && updateAccount(data))}
+        />
+      </FlexLayout>
       <FlexLayout flexDirection="column" space={5} sx={{ width: '200px' }}>
         <Text color="dimmed" onClick={showModal}>
           Delete account
